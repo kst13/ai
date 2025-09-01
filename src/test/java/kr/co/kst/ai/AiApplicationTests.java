b4a5bff00609d567b841c7727598ac15bc2b077b
kst <kst@osstem.com>
2025-09-01 17:18:08 +0900

    init

---
+++ src/test/java/kr/co/kst/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kst.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
